d4a4010d30fe6e952151548d9d904e675a8bfe57
Saad Siddiqui <saad@sids92.co>
2016-10-25 01:06:04 -0500

    Completed full functionality v1.0

--- server.py
+++ server.py
@@ -7,14 +7,33 @@ app = Flask(__name__)
 PORT = 443
 HOST = '0.0.0.0'
 
-@app.route("/tv", methods=['POST', 'GET'])
+@app.route("/", methods=['POST'])
 def index():
+    intent = request.json['request']['intent']['name']
     switch = request.json['request']['intent']['slots']['Switch']['value']
     isOn = True if switch == 'on' else False
     if isOn:
-        print(system('irsend SEND_ONCE TV P-On'))
+        if intent == 'ControlTV':
+            print(system('irsend SEND_ONCE TV P-On'))
+        elif intent == 'ControlAVR':
+            print(system('irsend SEND_ONCE HK KEY_POWER'))
+        elif intent == 'ControlXF':
+            print(system('irsend SEND_ONCE XF KEY_POWER'))
+        elif intent == 'ControlALL':
+            print(system('irsend SEND_ONCE TV P-On'))
+            print(system('irsend SEND_ONCE HK KEY_POWER'))
+            print(system('irsend SEND_ONCE XF KEY_POWER'))
     else:
-        print(system('irsend SEND_ONCE TV P-Off'))
+        if intent == 'ControlTV':
+            print(system('irsend SEND_ONCE TV P-Off'))
+        elif intent == 'ControlAVR':
+            print(system('irsend SEND_ONCE HK off'))
+        elif intent == 'ControlXF':
+            print(system('irsend SEND_ONCE XF KEY_POWER'))
+        elif intent == 'ControlALL':
+            print(system('irsend SEND_ONCE TV P-Off'))
+            print(system('irsend SEND_ONCE HK off'))
+            print(system('irsend SEND_ONCE XF KEY_POWER'))
     response = {}
     response['version'] = '1.0'
     response['response'] = {
@@ -26,15 +45,7 @@ def index():
     }
     return jsonify(response)
 
-@app.route("/tv/on")
-def tvOn():
-    print(system('irsend SEND_ONCE TV P-On'))
-    return jsonify({'success':1, 'message':'TV successfully turned on'})
 
-@app.route("/tv/off")
-def tvOff():
-    print(system('irsend SEND_ONCE TV P-Off'))
-    return jsonify({'success':1, 'message':'TV successfully turned off'})
 
 if __name__ == "__main__":
     context = ('ssl/allcert.crt', 'ssl/cert.key')
