d780c059e7a5bbd217136b63fafc9950fec70a93
Saad Siddiqui <saad@sids92.co>
2016-10-26 04:00:37 -0500

    Edited intent names

--- server.py
+++ server.py
@@ -12,30 +12,24 @@ HOST = '0.0.0.0'
 def index():
     pprint.pprint(request.json)
     intent = request.json['request']['intent']['name']
-    switch = request.json['request']['intent']['slots']['Switch']['value']
-    isOn = True if switch == 'on' else False
-    if isOn:
-        if intent == 'ControlTV':
-            print(system('irsend SEND_ONCE TV-Discrete P-On'))
-        elif intent == 'ControlAVR':
-            print(system('irsend SEND_ONCE HK KEY_POWER'))
-        elif intent == 'ControlXF':
-            print(system('irsend SEND_ONCE XF KEY_POWER'))
-        elif intent == 'ControlALL':
-            print(system('irsend SEND_ONCE TV-Discrete P-On'))
-            print(system('irsend SEND_ONCE HK KEY_POWER'))
-            print(system('irsend SEND_ONCE XF KEY_POWER'))
-    else:
-        if intent == 'ControlTV':
-            print(system('irsend SEND_ONCE TV-Discrete P-Off'))
-        elif intent == 'ControlAVR':
-            print(system('irsend SEND_ONCE HK off'))
-        elif intent == 'ControlXF':
-            print(system('irsend SEND_ONCE XF KEY_POWER'))
-        elif intent == 'ControlALL':
-            print(system('irsend SEND_ONCE TV-Discrete P-Off'))
-            print(system('irsend SEND_ONCE HK off'))
-            print(system('irsend SEND_ONCE XF KEY_POWER'))
+    if intent == 'ControlTVPower':
+        switch = request.json['request']['intent']['slots']['Switch']['value']
+        code = 'P-On' if switch == 'on' else 'P-Off'
+        print(system('irsend SEND_ONCE TV-Discrete {}'.format(code)))
+    elif intent == 'ControlAVRPower':
+        switch = request.json['request']['intent']['slots']['Switch']['value']
+        code = 'KEY_POWER' if switch == 'on' else 'off'
+        print(system('irsend SEND_ONCE HK {}'.format(code)))
+    elif intent == 'ControlXFPower':
+        print(system('irsend SEND_ONCE XF KEY_POWER'))
+    elif intent == 'ControlALLPower':
+        switch = request.json['request']['intent']['slots']['Switch']['value']
+        code = 'P-On' if switch == 'on' else 'P-Off'
+        print(system('irsend SEND_ONCE TV-Discrete {}'.format(code)))
+        code = 'KEY_POWER' if switch == 'on' else 'off'
+        print(system('irsend SEND_ONCE HK {}'.format(code)))
+
+
     response = {}
     response['version'] = '1.0'
     response['response'] = {
